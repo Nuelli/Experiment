eb3fcd86feb5951abfddfd8cdec704ebce1eb0de
Nuelli <emmanuelabwao@gmail.com>
2024-06-04 10:11:56 +0300

    second Commit to BitBucket

--- src/main/java/com/example/demo/NoBsSpringBootApplication.java
+++ src/main/java/com/example/demo/NoBsSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NoBsSpringBootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Welcome to my first integration");
 		SpringApplication.run(NoBsSpringBootApplication.class, args);
 	}
 
